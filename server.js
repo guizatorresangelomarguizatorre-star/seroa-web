f77a1642fade38fb97ca18b38e24f284959f5c4e
Angel Omar Guiza Torres <guizatorresangelomarguizatorre@gmail.com>
2026-04-28 21:28:41 -0600

    Correcion de Rutas

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path'); // Herramienta nativa de Node para leer carpetas
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Le decimos explícitamente dónde buscar los archivos estáticos (CSS, JS, imágenes)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-
+// Ruta de emergencia: Si entran a la raíz '/', mándalos al index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor de Seroa corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor de Seroa corriendo en el puerto ${PORT}`);
 });
