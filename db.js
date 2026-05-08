5a19fb0a87604d8598dca1963c5a4f6083edc780
Angel Omar Guiza Torres <guizatorresangelomarguizatorre@gmail.com>
2026-05-07 20:08:37 -0600

    Fix: Habilitar protocolo SSL para conexion DB

--- db.js
+++ db.js
@@ -1,14 +1,26 @@
 const mysql = require('mysql2');
 
-// Le decimos que use la URL completa directamente desde las variables de Railway
-const pool = mysql.createPool(process.env.MYSQL_URL);
+// Conexión pública con encriptación SSL obligatoria
+const pool = mysql.createPool({
+    host: 'switchback.proxy.rlwy.net',
+    user: 'root',
+    password: 'DYFgQxnovIUFCHrgUPusszxMGMYfUrux',
+    database: 'railway',
+    port: 35115,
+    ssl: {
+        rejectUnauthorized: false // <--- ¡LA LLAVE MÁGICA PARA LA NUBE!
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 pool.getConnection((err, connection) => {
     if (err) {
-        console.error('Error conectando a MySQL:', err.message);
+        console.error('Error crítico conectando a MySQL:', err.message);
         return;
     }
-    console.log('¡Conexión exitosa a la base de datos interna de Railway!');
+    console.log('¡Conexión segura SSL exitosa a la bóveda de Seroa!');
     connection.release();
 });
 
